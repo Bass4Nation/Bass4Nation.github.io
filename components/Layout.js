ee749f6e8c97e983dd7a1f3ec03deafc72d53a6a
Kristoffer Snopestad Søderkvist <bass4nation@gmail.com>
2023-01-12 16:04:44 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 import style from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);  // darkMode is a boolean and is used to toggle the dark mode on and off
 
   return (
     <section className={darkMode ? style.darkmode : style.root}>
